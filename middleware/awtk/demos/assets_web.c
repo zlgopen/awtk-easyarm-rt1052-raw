47c1269ffd75749161662dbd904cc6a346026f4a
chentan <chentan@zlg.cn>
2020-06-17 11:48:14 +0800

    update awtk to awtk-v1.4+rc.1+fix.1

--- middleware/awtk/demos/assets_web.c
+++
@@ -1,9 +0,0 @@
-#include "awtk.h"
-#include "base/assets_manager.h"
-
-ret_t assets_init(void) {
-  assets_manager_t* am = assets_manager();
-
-  tk_init_assets();
-  return RET_OK;
-}
--- middleware/awtk/format.sh
+++
@@ -1,6 +0,0 @@
-find src tools demos tests -name \*.c -exec clang-format -i {} \;
-find src tools demos tests -name \*.h -exec clang-format -i {} \;
-find src tools demos tests -name \*.cc -exec clang-format -i {} \;
-find src tools demos tests -name \*.cpp -exec clang-format -i {} \;
-find src tools demos tests -name \*.inc  -exec clang-format -i {} \;
-
